52d7ff2a8a1b4e994d3bc911a24bcc3bed4b0ffd
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-20 04:24:30 +0530

    - added schemas

---
+++ server/api/models/project.js
@@ -0,0 +1,9 @@
+// Project Model
+
+const mongoose = require('mongoose')
+
+const projectSchema = new mongoose.Schema({
+    name: { type: String },
+    description: { type: String },
+    isActive: { type: Boolean, default: true }
+})
---
+++ server/api/models/standup.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const standupSchema = new mongoose.Schema({
+    teamMember: { type: String },
+    project: { type: String },
+    workYesterday: { type: String },
+    workToday: { type: String },
+    impedimentS: { type: String },
+    createdOn: { type: Date, default: Date.now }
+})
+
+
+
+// // Disabled _id schema example
+// const noIdSchema = new mongoose.Schema(
+//     { name: String },
+//     { _id: false }
+// )
+
+// // User Schema.add() example
+// let exampleSchema = new mongoose.Schema
+
+// //exampleSchema.add({ teamMember: String })
+// const useFullName = true // Some business logic here 
+
+// if (useFullName) {
+//     exampleSchema.add({
+//         teamMember: {
+//             first: String,
+//             last: String
+//         }
+//     })
+// } else {
+//     exampleSchema.add({ teamMember: String })
+// }
