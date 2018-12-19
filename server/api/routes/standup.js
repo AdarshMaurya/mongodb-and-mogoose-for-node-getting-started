31228bc12f7502f7d7a29090b460c28c06791008
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-20 04:34:18 +0530

    - moved the folder models from api to server directory

--- server/api/routes/standup.js
+++ server/api/routes/standup.js
@@ -1,8 +1,13 @@
 
-
+const Standup = require('../../models/standup')
 module.exports = function (router) {
     // Get: the 12 newest stand-up meeting notes
     router.get('/standup', function (req, res) {
 
+    })
+
+    // POST: Get new meeting note document
+    router.post('/standup', function (req, res) {
+        let note = new Standup()
     })
 }
